5c2790a0b2caca20b592067cfd6fd398bcda3b83
MasahiroMurakami <kodama.p19870112@gmail.com>
2021-04-27 11:33:27 +0900

    訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+          validates :nickname
+          validates :email
+          validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }, length: { minimum: 6 }
+          validates :birth_day
+          validates :address
+          validates :phone_number, format: {with: /\A\d{10,11}\z/ },numericality: { only_integer: true}
+          with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+            validates :family_name
+            validates :first_name
+          end
+          with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+            validates :family_name_kana
+            validates :first_name_kana
+          end
+        end
+        has_many :buyer_histories
+        has_many :items, dependent: :destroy
 end
